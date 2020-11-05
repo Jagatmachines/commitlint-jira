4e9c1182494e165c8c5ae425fd50081f71ff5582
bigya <bigya@fusemachines.com>
2020-11-05 18:59:50 +0545

    check commit

---
+++ packages/test-commits/commitlint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: ['commitlint-plugin-jira-rules'],
+  rules: {
+    'jira-task-id-min-length': [2, 'always', 4],
+    'jira-task-id-max-length': [2, 'always', 7],
+  },
+  parserPreset: {
+    parserOpts: {
+      issuePrefixes: ['FUSEAI-'],
+    },
+  },
+};
