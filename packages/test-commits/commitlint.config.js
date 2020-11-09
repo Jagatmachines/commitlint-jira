3b9e21a1192b1d4567cd84502f24244422f2ea85
bigya <bigya@fusemachines.com>
2020-11-09 18:34:52 +0545

    [CL]ERER-00 hello lol

--- packages/test-commits/commitlint.config.js
+++ packages/test-commits/commitlint.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   plugins: ['commitlint-plugin-jira-rules'],
+  extends: ['jira'],
   rules: {
     'jira-task-id-min-length': [2, 'always', 4],
     'jira-task-id-max-length': [2, 'always', 7],
@@ -9,4 +10,4 @@ module.exports = {
       issuePrefixes: ['FUSEAI-'],
     },
   },
-};
+}
