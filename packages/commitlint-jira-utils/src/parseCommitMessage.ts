451740e2f8fb3752a3a6506609d576b678421aca
bigya <bigya@fusemachines.com>
2020-11-11 18:27:47 +0545

    [FIX] TASK-0 test commit

--- packages/commitlint-jira-utils/src/parseCommitMessage.ts
+++ packages/commitlint-jira-utils/src/parseCommitMessage.ts
@@ -4,7 +4,7 @@ import {
   COMMIT_MESSAGE_SEPARATOR,
   COMMIT_STATUS_SEPARATORS,
   COMMIT_TASK_IDS_SEPARATOR,
-  COMMIT_TASK_STATUS_PATTERN,
+  COMMIT_TASK_STATUS_PATTERN
 } from './commitlintJiraConstants'
 
 const parseCommitMessage: TParseCommitMessage = rawCommitMessage => {
@@ -56,7 +56,7 @@ const parseCommitMessage: TParseCommitMessage = rawCommitMessage => {
   const commitStatus = rawCommitStatus.length
     ? rawCommitStatus[0].replace(COMMIT_STATUS_SEPARATORS.start, '').trim()
     : ''
-
+console.log({rawCommitStatus, commitStatus})
   const commitTaskIds = commitHeader
     .split(COMMIT_TASK_IDS_SEPARATOR)
     .map(taskId => taskId.trim())
