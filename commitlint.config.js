b0cf2f21cd91df754db8e59bff55bfdf975a2e7d
bigya <bigya@fusemachines.com>
2020-11-11 15:43:21 +0545

    [UPDATE] TASK-01 Update commit message parsing code

--- commitlint.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: ['commitlint-plugin-jira-rules'],
-  extends: ['jira'],
-  rules: {
-    'jira-task-id-min-length': [2, 'always', 4],
-    'jira-task-id-max-length': [2, 'always', 7],
-  },
-}
--- packages/commitlint-plugin-jira-rules/src/rules/jiraTaskIdEmptyRuleResolver.ts
+++ packages/commitlint-plugin-jira-rules/src/rules/jiraTaskIdEmptyRuleResolver.ts
@@ -1,6 +1,5 @@
 import {
-  parseCommitMessage,
-  commitlintJiraConstants,
+  commitlintJiraConstants, parseCommitMessage
 } from 'commitlint-jira-utils'
 import { TRuleResolver } from '../../@types'
 
@@ -11,6 +10,7 @@ const jiraTaskIdEmptyRuleResolver: TRuleResolver = parsed => {
   const commitMessage = parseCommitMessage(rawCommitMessage)
 
   const isRuleValid = commitMessage.commitTaskIds.length > 0
+  console.log({rawCommitMessage, commitMessage, isRuleValid, commitTaskIds:commitMessage.commitTaskIds})
   return [
     isRuleValid,
     `the commit message must provide minimum one task id followed by (${commitlintJiraConstants.COMMIT_MESSAGE_SEPARATOR}) symbol, if task not have an id use a conventional task id e.g: "FUSEAI-0000${commitlintJiraConstants.COMMIT_MESSAGE_SEPARATOR} My commit message"`,
